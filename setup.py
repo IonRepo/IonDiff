c450ed276fd70597944a0f7035290729b82f97ac
Cibrán López Álvarez <49532916+CibranLopez@users.noreply.github.com>
2024-07-12 17:53:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('LICENSE') as f:
 setup(
   name = 'IonDiff',
   packages = ['IonDiff'],
-  version = '1.4',
+  version = '1.5',
   license=license,
   description = 'Unsupervised identification and analysis of ion-hopping events in solid state electrolytes.',
   long_description=readme,
